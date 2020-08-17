093a09c945f9d0ab0bd3e5656fd5cf9885f52aaf
Joao Gui <joaoguidev@gmail.com>
2020-08-17 11:33:19 -0700

    hey kirkland. This is the last update

--- src/card.js
+++ src/card.js
@@ -6,16 +6,23 @@ export default class Card {
             x: 100,
             y: 100,
         };
+        this.isDragging = false;
+        document.addEventListener("mousedown", event => {
+            this.position.x = event.clientX;
+            this.position.y = event.clientY;
+        })
     }
 
     draw(context) {
-
-        
         context.fillRect(
             this.position.x,
             this.position.y,
             this.width,
             this.heigth
         );
+    }
+
+    update(deltaTime){
+        
     }
 }
